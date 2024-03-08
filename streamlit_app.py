c22c2aa7cac4f7443d74a6e8c44cfb92236e7f49
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2024-03-08 11:15:53 -0500

    update repo

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ st.write('The on your smoothie will be:', name_on_order)
 
 
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 
